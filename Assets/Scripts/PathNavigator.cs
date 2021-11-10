32750bb2f4171a40e8664c895bdba9dfc897bf85
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-11-11 00:07:11 +0100

    Added player movement + lookat test
    - Player can now move over the bezier curve from start to end
    - Player now rotates facing back to 0,0,0 rotation when reaching the level position
    
    TO DO:
    - Fix rotation of the player on the bezier curve so the player actually looks in the direction the path is going
    - Implement check to see if the player reached another level node so it can stop following the path and just positions itself on the level node correctly
    - Being able to move back and forward on the bezier curve between two level nodes

--- Assets/Scripts/PathNavigator.cs
+++ Assets/Scripts/PathNavigator.cs
@@ -5,13 +5,68 @@ using UnityEngine.InputSystem;
 
 public class PathNavigator : MonoBehaviour
 {
+    #region Variables
+    //Navigation
+    public PathLayout CurrentPath;
+    public float Duration;
+    public float Progress;
+    public float PositioningSpeed;
+    public bool GoingForward;
+
+    private Vector3 CurrentPosition;
+
+    //Player input
     public Vector2 DirectionInput;
     public bool Confirm;
+    #endregion
+
+    private void Start()
+    {
+        PositionPlayerOnCurve();
+        StartCoroutine(PositionPlayerOnLevel());
+    }
+
+    private void PositionPlayerOnCurve()
+    {
+        if (CurrentPath != null)
+        {
+            CurrentPosition = CurrentPath.GetPoint(Progress);
+            transform.position = CurrentPosition;
+        }
+    }
+
+    IEnumerator MovePlayer()
+    {
+        while(Progress < 1f)
+        {
+            Progress += Time.deltaTime / Duration;
+            if(Progress > 1f)
+            {
+                Progress = 1f;
+            }
+            CurrentPosition = CurrentPath.GetPoint(Progress);
+            transform.position = CurrentPosition;
+            transform.LookAt(CurrentPosition + CurrentPath.GetPoint(Progress));
+            yield return Progress;
+        }
+    }
+
+    private IEnumerator PositionPlayerOnLevel()
+    {
+        while (transform.rotation != Quaternion.Euler(0, 0, 0))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * PositioningSpeed);
+            yield return transform.rotation;
+        }
+        transform.rotation = Quaternion.identity;
+        StopCoroutine(PositionPlayerOnLevel());
+    }
 
     #region Inputsystem
     public void OnDirection(InputAction.CallbackContext Context)
     {
         DirectionInput = Context.ReadValue<Vector2>();
+        StartCoroutine(MovePlayer());
     }
 
     public void OnConfirm(InputAction.CallbackContext Context)
