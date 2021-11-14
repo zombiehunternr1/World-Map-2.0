6eb9fd6530bd2de41ea1333e92d83cf881460857
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-11-14 16:39:57 +0100

    Path Decoration test implemented
    - Created a new script called PathDecoration that displays a path item along the set path line with an set ofset between them. It also has a wait time before displaying it one by one for the first time or instantly when it isn't.
    
    TO DO:
    - Fix issue where the path decoration object isn't properly rotate on the pathway

---
+++ Assets/Scripts/Worldmap/PathDecoration.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDecoration : MonoBehaviour
+{
+    public int WaitBeforePathDecorating = 3;
+    public float WaitDisplayNextPathDecoration;
+    public int Frequency;
+    public float Spacing = 1;
+    public Transform[] DecorationItems;
+    //[HideInInspector]
+    public bool FirstTime = true;
+    private PathLayout PathToDecorate;
+
+    private void Start()
+    {
+        PathToDecorate = GetComponent<PathLayout>();
+        if (PathToDecorate.Unlocked)
+        {
+            StartCoroutine(DecoratePath());
+        }
+    }
+
+    private IEnumerator DecoratePath()
+    {
+        if (Frequency <= 0 || DecorationItems == null || DecorationItems.Length == 0)
+        {
+            yield return null;
+        }
+        float StepSize = Spacing / (Frequency * DecorationItems.Length);
+        if (FirstTime)
+        {
+            yield return new WaitForSeconds(WaitBeforePathDecorating);
+        }
+        for (int p = 0, f = 0; f < Frequency; f++)
+        {
+            for (int i = 0; i < DecorationItems.Length; i++, p++)
+            {
+                Transform PathDecorationItem = Instantiate(DecorationItems[i]) as Transform;
+                Vector3 Position = PathToDecorate.GetPoint(p * StepSize);
+                PathDecorationItem.transform.localPosition = Position;
+                PathDecorationItem.transform.LookAt(Position + PathToDecorate.GetDirection(p * StepSize));
+                PathDecorationItem.transform.parent = gameObject.transform;
+                if (FirstTime)
+                {
+                    yield return new WaitForSeconds(WaitDisplayNextPathDecoration);
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Worldmap/PathNavigator.cs
+++ Assets/Scripts/Worldmap/PathNavigator.cs
@@ -37,68 +37,6 @@ public class PathNavigator : MonoBehaviour
         }
     }
 
-    IEnumerator MovePlayer()
-    {
-        if (Progress < 1)
-        {
-            while (Progress < 1f)
-            {
-                Progress += Time.deltaTime / Duration;
-                if (Progress > 1f)
-                {
-                    Progress = 1f;
-                }
-                CurrentPosition = CurrentPath.GetPoint(Progress);
-                transform.localPosition = CurrentPosition;
-                transform.LookAt(CurrentPosition + CurrentPath.GetDirection(Progress));
-                yield return Progress;
-            }
-        }
-        else
-        {
-            while (Progress > 0)
-            {
-                Progress -= Time.deltaTime / Duration;
-                if (Progress < 0)
-                {
-                    Progress = 0;
-                }
-                CurrentPosition = CurrentPath.GetPoint(Progress);
-                transform.localPosition = CurrentPosition;
-                transform.LookAt(CurrentPosition - CurrentPath.GetDirection(Progress));
-                yield return Progress;
-            }
-        }
-    }
-
-    private IEnumerator PositionPlayerOnLevel(Transform LevelPosition)
-    {
-        while (transform.localPosition != LevelPosition.position)
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, LevelPosition.position, Time.deltaTime * PositioningSpeed);
-            yield return null;
-        }
-        while (transform.localRotation != Quaternion.Euler(0, 180, 0))
-        {
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 180, 0), Time.deltaTime * PositioningSpeed);
-            yield return transform.localRotation;
-        }
-        transform.localRotation = Quaternion.Euler(0, 180, 0);
-        StopCoroutine(PositionPlayerOnLevel(LevelPosition));
-    }
-    #endregion
-
-    private void OnTriggerEnter(Collider other)
-    {
-        StopAllCoroutines();
-        StartCoroutine(PositionPlayerOnLevel(other.transform));
-        CurrentLevel = other.GetComponent<LevelNodeData>();
-        IsMoving = false;
-        Progress = 0;
-        Debug.Log(CurrentLevel.LevelInfo.LevelName);
-        Debug.Log(CurrentLevel.LevelInfo.LevelNumber);
-    }
-
     private void CheckDirection(Vector2 SelectedDirection)
     {
         //Up
@@ -241,14 +179,14 @@ public class PathNavigator : MonoBehaviour
                     IsMoving = true;
                     int Index = CurrentLevel.AvailableConnectedPaths.IndexOf(i);
                     int DirectionValue = (int)LevelNodeData.Direction.LEFT;
-                    if(CurrentPath != CurrentLevel.ConnectedPaths[Index])
+                    if (CurrentPath != CurrentLevel.ConnectedPaths[Index])
                     {
                         CurrentPath = CurrentLevel.ConnectedPaths[Index];
                         if (CurrentPath.Unlocked)
                         {
-                            foreach(var j in CurrentLevel.IsPreviousPath)
+                            foreach (var j in CurrentLevel.IsPreviousPath)
                             {
-                                if(((int)j) == DirectionValue)
+                                if (((int)j) == DirectionValue)
                                 {
                                     Progress = 1;
                                     StartCoroutine(MovePlayer());
@@ -361,6 +299,68 @@ public class PathNavigator : MonoBehaviour
                 }
             }
         }
+    }
+
+    private IEnumerator MovePlayer()
+    {
+        if (Progress < 1)
+        {
+            while (Progress < 1f)
+            {
+                Progress += Time.deltaTime / Duration;
+                if (Progress > 1f)
+                {
+                    Progress = 1f;
+                }
+                CurrentPosition = CurrentPath.GetPoint(Progress);
+                transform.localPosition = CurrentPosition;
+                transform.LookAt(CurrentPosition + CurrentPath.GetDirection(Progress));
+                yield return Progress;
+            }
+        }
+        else
+        {
+            while (Progress > 0)
+            {
+                Progress -= Time.deltaTime / Duration;
+                if (Progress < 0)
+                {
+                    Progress = 0;
+                }
+                CurrentPosition = CurrentPath.GetPoint(Progress);
+                transform.localPosition = CurrentPosition;
+                transform.LookAt(CurrentPosition - CurrentPath.GetDirection(Progress));
+                yield return Progress;
+            }
+        }
+    }
+
+    private IEnumerator PositionPlayerOnLevel(Transform LevelPosition)
+    {
+        while (transform.localPosition != LevelPosition.position)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, LevelPosition.position, Time.deltaTime * PositioningSpeed);
+            yield return transform.localPosition;
+        }
+        while (transform.localRotation != Quaternion.Euler(0, 180, 0))
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 180, 0), Time.deltaTime * PositioningSpeed);
+            yield return transform.localRotation;
+        }
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+        StopCoroutine(PositionPlayerOnLevel(LevelPosition));
+    }
+    #endregion
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StopAllCoroutines();
+        StartCoroutine(PositionPlayerOnLevel(other.transform));
+        CurrentLevel = other.GetComponent<LevelNodeData>();
+        IsMoving = false;
+        Progress = 0;
+        Debug.Log(CurrentLevel.LevelInfo.LevelName);
+        Debug.Log(CurrentLevel.LevelInfo.LevelNumber);
     }
 
     #region Inputsystem
