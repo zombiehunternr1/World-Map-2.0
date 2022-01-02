3fefdea6a42498310fbacd923e6d8c78ef0e4796
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2022-01-02 18:08:35 +0100

    Code cleanup bezier editor
    Noticed that the function ShowDirection was absolute, so I removed it from the script.

--- Assets/Scripts/Editor/PathLayoutEditor.cs
+++ Assets/Scripts/Editor/PathLayoutEditor.cs
@@ -26,18 +26,7 @@ public class PathLayoutEditor : Editor {
 		Handles.DrawLine(p0, p1);
 		Handles.DrawLine(p2, p3);
 		
-		ShowDirections();
 		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
-	}
-
-	private void ShowDirections () {
-		Handles.color = Color.green;
-		Vector3 point = curve.GetPoint(0f);
-		Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
-		for (int i = 1; i <= lineSteps; i++) {
-			point = curve.GetPoint(i / (float)lineSteps);
-			Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
-		}
 	}
 
 	private Vector3 ShowPoint (int index) {
