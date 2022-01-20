1dc461bb42cb70bf35466ca94060721cd607dd10
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2022-01-20 15:40:50 +0100

    Improved anim trigger
    Removed the none enum value as well as the check for it to return nothing since not all paths have gaps between them.

--- Assets/Scripts/Worldmap/AnimTrigger.cs
+++ Assets/Scripts/Worldmap/AnimTrigger.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class AnimTrigger : MonoBehaviour
 {
-    public enum animationOptions { None, Jump };
+    public enum animationOptions { Jump };
     public animationOptions animOptions;
 
     private void OnTriggerEnter(Collider other)
     {
         PathNavigator pathNavigator = other.GetComponent<PathNavigator>();
-        if(animOptions == animationOptions.None)
-        {
-            return;
-        }
-        else if(animOptions == animationOptions.Jump)
+        if(animOptions == animationOptions.Jump)
         {
             pathNavigator.playerAnimator.Play("Jump");
         }
