e8346043e80f1ad657413a655258f6d59961f460
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2022-01-24 14:56:46 +0100

    Improved path layout and editor scripts
    - Inside the path layout script I've created an int function called CurveCount that returns the total amounts inside the list devided by three. This will be used to calculate the players positions on the path
    - Inside the get point and get velocity function they now both calculate the players next position based on the result of the amount of points in the list. That way we can add multiple curves to a path and the player keeps moving along it
    - Created an add curve function that adds three new curve points to the array that the player can follow along on
    - Created an remove last curve function that removes the last three curve points that have been added to the array
    - Inside the path layout editor I've created two constant float values for handle size and pick size. These values are used to make it more easier to move the curve inside the scene view
    - Created an selected index value and set this to -1. This will be used to determind which curve point we want to move
    - Created an override on inspector gui function that adds a button that allows you to add move curves to  the same path. Inside this function I've also made an extra check that if the length of curve points is greater then four, the remove last curve button should be displayed.
    - Inside the on scene gui I loop over the curve point array and add them to the shouldpoint requirement count. This is to display the new curve points after the last already existing point, rather then overlapping them.
    - Inside the show point function I've added in an extra check to see which handle is selected and set this value to the selected index value. That way I can visually see inside the scene view which point I'm currently moving around.

--- Assets/Scripts/Editor/PathLayoutEditor.cs
+++ Assets/Scripts/Editor/PathLayoutEditor.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 [CustomEditor(typeof(PathLayout))]
 public class PathLayoutEditor : Editor {
 
-	private const int lineSteps = 10;
-	private const float directionScale = 0.5f;
+	private const float handleSize = 0.04f;
+	private const float pickSize = 0.06f;
+	private int selectedIndex = -1;
 
 	private PathLayout curve;
 	private Transform handleTransform;
 	private Quaternion handleRotation;
+
+	public override void OnInspectorGUI()
+    {
+		DrawDefaultInspector();
+		curve = target as PathLayout;
+		if(GUILayout.Button("Add Curve"))
+        {
+			Undo.RecordObject(curve, "Add Curve");
+			curve.AddCurve();
+			EditorUtility.SetDirty(curve);
+        }
+		if(curve.points.Length > 4)
+        {
+			if (GUILayout.Button("Remove Last Curve"))
+			{
+				Undo.RecordObject(curve, "Remove Last Curve");
+				curve.RemoveLastCurve();
+				EditorUtility.SetDirty(curve);
+			}
+		}
+	}
 
 	private void OnSceneGUI () {
 		curve = target as PathLayout;
@@ -18,22 +40,35 @@ public class PathLayoutEditor : Editor {
 			handleTransform.rotation : Quaternion.identity;
 		
 		Vector3 p0 = ShowPoint(0);
-		Vector3 p1 = ShowPoint(1);
-		Vector3 p2 = ShowPoint(2);
-		Vector3 p3 = ShowPoint(3);
-		
-		Handles.color = Color.gray;	
-		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+		for(int i = 1; i < curve.points.Length; i += 3)
+        {
+			Vector3 p1 = ShowPoint(i);
+			Vector3 p2 = ShowPoint(i + 1);
+			Vector3 p3 = ShowPoint(i + 2);
+			Handles.color = Color.gray;
+			Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+			p0 = p3;
+		}	
 	}
 
 	private Vector3 ShowPoint (int index) {
 		Vector3 point = handleTransform.TransformPoint(curve.points[index]);
-		EditorGUI.BeginChangeCheck();
-		point = Handles.DoPositionHandle(point, handleRotation);
-		if (EditorGUI.EndChangeCheck()) {
-			Undo.RecordObject(curve, "Move Point");
-			EditorUtility.SetDirty(curve);
-			curve.points[index] = handleTransform.InverseTransformPoint(point);
+		Handles.color = Color.white;
+		float size = HandleUtility.GetHandleSize(point);
+		if(Handles.Button(point, handleRotation, size * handleSize, size * pickSize, Handles.DotHandleCap))
+        {
+			selectedIndex = index;
+        }
+		if(selectedIndex == index)
+        {
+			EditorGUI.BeginChangeCheck();
+			point = Handles.DoPositionHandle(point, handleRotation);
+			if (EditorGUI.EndChangeCheck())
+			{
+				Undo.RecordObject(curve, "Move Point");
+				EditorUtility.SetDirty(curve);
+				curve.points[index] = handleTransform.InverseTransformPoint(point);
+			}
 		}
 		return point;
 	}
--- Assets/Scripts/Worldmap/PathLayout.cs
+++ Assets/Scripts/Worldmap/PathLayout.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 public class PathLayout : MonoBehaviour {
 
@@ -10,15 +11,74 @@ public class PathLayout : MonoBehaviour {
 		get;
 		set;
     }
+
+	private int CurveCount
+    {
+        get
+        {
+			return (points.Length - 1) / 3;
+        }
+    }
+
 	public Vector3 GetPoint (float t) {
-		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+		int i;
+		if(t >= 1)
+        {
+			t = 1;
+			i = points.Length - 4;
+        }
+        else
+        {
+			t = Mathf.Clamp01(t) * CurveCount;
+			i = (int)t;
+			t -= i;
+			i *= 3;
+        }
+		return transform.TransformPoint(Bezier.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
 	}
 	
 	public Vector3 GetVelocity (float t) {
-		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
+		int i;
+		if (t >= 1)
+		{
+			t = 1;
+			i = points.Length - 4;
+		}
+		else
+		{
+			t = Mathf.Clamp01(t) * CurveCount;
+			i = (int)t;
+			t -= i;
+			i *= 3;
+		}
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
 	}
 	
 	public Vector3 GetDirection (float t) {
 		return GetVelocity(t).normalized;
+	}
+
+	public void AddCurve()
+    {
+		Vector3 point = points[points.Length - 1];
+		Array.Resize(ref points, points.Length + 3);
+		point.x += 1;
+		points[points.Length - 3] = point;
+		point.x += 1;
+		points[points.Length - 2] = point;
+		point.x += 1;
+		points[points.Length - 1] = point;
+	}
+
+	public void RemoveLastCurve()
+    {
+		Vector3 point = points[points.Length - 1];
+		Array.Resize(ref points, points.Length - 3);
+		point.x -= 1;
+		points[points.Length - 1] = point;
+		point.x -= 1;
+		points[points.Length - 1] = point;
+		point.x -= 1;
+		points[points.Length - 1] = point;
 	}
 }
