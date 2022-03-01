cabab030cb96c01f7da3dc9270acf94f19775ef1
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2022-03-01 20:33:58 +0100

    Improved path display animation
    - Added in animation to display the path and ladder
    - Added in a curve path model for testing purposes
    
    TO DO:
    Find a way to display a curve path animation just like that has been done with a straight path and ladder

--- Assets/Scripts/Worldmap/AnimTrigger.cs
+++ Assets/Scripts/Worldmap/AnimTrigger.cs
@@ -6,46 +6,58 @@ public class AnimTrigger : MonoBehaviour
 {
     public enum animationOptions { Jump, ClimbingUp, climbingDown};
     public animationOptions animOptions;
+    private bool canTrigger = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        PathNavigator pathNavigator = other.GetComponent<PathNavigator>();
-        switch (animOptions)
+        if (canTrigger)
         {
-            case animationOptions.Jump:
-                pathNavigator.playerAnimator.Play("Jump");
-                break;
-            case animationOptions.ClimbingUp:
-                if (pathNavigator.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ClimbingLadderDown") && !pathNavigator.isAnimOnLocation)
-                {
-                    pathNavigator.isVertical = false;
-                    pathNavigator.isAnimOnLocation = true;
-                    pathNavigator.playerAnimator.Play("ClimbDownDone");
-                }
-                else
-                {
-                    pathNavigator.isRunningAnim = false;
-                    pathNavigator.isAnimOnLocation = true;
-                    pathNavigator.isVertical = true;
-                    pathNavigator.playerAnimator.Play("ClimbUpLadder");
-                }
-                break;
-            case animationOptions.climbingDown:
-                if (pathNavigator.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ClimbingLadderUp") && !pathNavigator.isAnimOnLocation)
-                {
-                    pathNavigator.isVertical = false;
-                    pathNavigator.isAnimOnLocation = true;
-                    pathNavigator.playerAnimator.Play("ClimbUpDone");
-                }
-                else
-                {
-                    pathNavigator.isRunningAnim = false;
-                    pathNavigator.isAnimOnLocation = true;
-                    pathNavigator.isVertical = true;
-                    pathNavigator.playerAnimator.Play("ClimbDownLadder");
-                }
-                break;
+            canTrigger = false;
+            PathNavigator pathNavigator = other.GetComponent<PathNavigator>();
+            switch (animOptions)
+            {
+                case animationOptions.Jump:
+                    pathNavigator.playerAnimator.Play("Jump");
+                    break;
+                case animationOptions.ClimbingUp:
+                    if (pathNavigator.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ClimbingLadderDown") && !pathNavigator.isAnimOnLocation)
+                    {
+                        pathNavigator.isVertical = false;
+                        pathNavigator.isAnimOnLocation = true;
+                        pathNavigator.playerAnimator.Play("ClimbDownDone");
+                    }
+                    else
+                    {
+                        pathNavigator.isRunningAnim = false;
+                        pathNavigator.isAnimOnLocation = true;
+                        pathNavigator.isVertical = true;
+                        pathNavigator.playerAnimator.Play("ClimbUpLadder");
+                    }
+                    break;
+                case animationOptions.climbingDown:
+                    if (pathNavigator.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ClimbingLadderUp") && !pathNavigator.isAnimOnLocation)
+                    {
+                        pathNavigator.isVertical = false;
+                        pathNavigator.isAnimOnLocation = true;
+                        pathNavigator.playerAnimator.Play("ClimbUpDone");
+                    }
+                    else
+                    {
+                        pathNavigator.isRunningAnim = false;
+                        pathNavigator.isAnimOnLocation = true;
+                        pathNavigator.isVertical = true;
+                        pathNavigator.playerAnimator.Play("ClimbDownLadder");
+                    }
+                    break;
+            }
+            StartCoroutine(cooldown());
         }
-
+    }
+     
+    private IEnumerator cooldown()
+    {
+        yield return new WaitForSeconds(2);
+        canTrigger = true;
+        StopAllCoroutines();
     }
 }
